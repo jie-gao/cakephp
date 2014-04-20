a3845e281fb9acca5f6c0cb7f0757ab6d21cdca4
jie-gao <c.gao.jie.j@hotmail.com>
2014-04-20 10:30:00 +0000

    commited 20140420

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -29,4 +29,38 @@ class PostsController extends AppController {
       $this->Session->setFlash(_('Unable to add your post.'));
     }
   }
+  public function edit($id = null) {
+    if(!$id) {
+      throw new NotFoundException(_('Invalid post'));
+    }
+    $post = $this->Post->findById($id);
+    if(!$post) {
+      throw new NotFoundException(_('Invalid post'));
+    }
+
+    if($this->request->is(array('post', 'put'))) {
+      $this->Post->id = $id;
+      if($this->Post->save($this->request->data)) {
+        $this->Session->setFlash(_('Your post has been updated.'));
+        return $this->redirect(array('action' => 'index'));
+      }
+      $this->Session->setFlash(_('Unable to update your post.'));
+    }
+ 
+    if(!$this->request->data) {
+      $this->request->data = $post;
+    }
+  }
+  public function delete($id) {
+    if($this->request->is('get')) {
+      throw new MethodNotAllowedException();
+    }
+
+    if ($this->Post->delete($id)) {
+      $this->Session->setFlash(
+        __('The post with id : %s has been deleted.', h($id))
+      );
+      return $this->redirect(array('action' => 'index'));
+    }
+  }
 }
